ce933231f3768c0b6caac77d4de7c9cf74de16ef
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2019-01-02 19:21:45 -0800

    Move Device.Send to its own file

---
+++ send.go
@@ -0,0 +1,68 @@
+package lifxlan
+
+import (
+	"context"
+	"fmt"
+	"net"
+)
+
+func (d *device) Send(
+	ctx context.Context,
+	conn net.Conn,
+	tagged TaggedHeader,
+	flags AckResFlag,
+	message MessageType,
+	payload []byte,
+) (seq uint8, err error) {
+	select {
+	default:
+	case <-ctx.Done():
+		err = ctx.Err()
+		return
+	}
+
+	var msg []byte
+	seq = d.NextSequence()
+	msg, err = GenerateMessage(
+		tagged,
+		d.Source(),
+		d.Target(),
+		flags,
+		seq,
+		message,
+		payload,
+	)
+	if err != nil {
+		return
+	}
+
+	select {
+	default:
+	case <-ctx.Done():
+		err = ctx.Err()
+		return
+	}
+
+	var n int
+	n, err = conn.Write(msg)
+	if err != nil {
+		return
+	}
+	if n < len(msg) {
+		err = fmt.Errorf(
+			"lifxlan.Device.Send: only wrote %d out of %d bytes",
+			n,
+			len(msg),
+		)
+		return
+	}
+
+	select {
+	default:
+	case <-ctx.Done():
+		err = ctx.Err()
+		return
+	}
+
+	return
+}
